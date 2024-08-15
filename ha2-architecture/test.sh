e9cc9b4cf4055ec6e59ee13cb3d71b0bd419e436
Wyatt Sullivan <wyatts@gmail.com>
2024-08-15 14:54:43 -0500

    removed old files

--- ha2-architecture/test.sh
+++
@@ -1,9 +0,0 @@
-SSM_PATH="/ec2/ips-03cd2d6d"
-ips_csv="`aws ssm get-parameter --name "${SSM_PATH}" | jq .Parameter.Value | sed -e 's/"//g'`"
-ips=($(echo $ips_csv $| tr "," "\n"))
-
-echo ${ips[0]}
-
-cat <<EOF > output
-ansible_host: `echo ${ips[0]}`
-EOF
