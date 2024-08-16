93858f21a6c4d0f9ffd18e4e0e8611713346834b
Wyatt Sullivan <wyatts@gmail.com>
2024-08-16 13:52:35 -0500

    typo

--- ha2-architecture/main.tf
+++ ha2-architecture/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region  = "${var.aws-region}"
   profile = "${var.aws-profile}"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "${var.aws-access-key}"
+  secret_key = "${var.aws-secret-key}"
 }
 
 # Use this to get unique names for stuff
